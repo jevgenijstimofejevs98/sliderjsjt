3f65aeaa58ecc3a6b303b2f883503bb70687c008
JevgenijsTimofejevs <JevgenijsTimofejevs@users.noreply.github.com>
2020-07-11 15:17:41 +0300

    Updates

---
+++ precache-manifest.233318c3218fc37ecfff168df8c7c799.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b99ead7b0757bfa05433fa02a1c5bd",
+    "url": "/sliderjsjt/index.html"
+  },
+  {
+    "revision": "410b40ea4b366a61564d",
+    "url": "/sliderjsjt/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7c7fb3933c3149b99449",
+    "url": "/sliderjsjt/static/css/main.3f93e042.chunk.css"
+  },
+  {
+    "revision": "410b40ea4b366a61564d",
+    "url": "/sliderjsjt/static/js/2.cee277cc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sliderjsjt/static/js/2.cee277cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7fb3933c3149b99449",
+    "url": "/sliderjsjt/static/js/main.c9a901b2.chunk.js"
+  },
+  {
+    "revision": "85b6a93860ff12394538",
+    "url": "/sliderjsjt/static/js/runtime-main.5decf227.js"
+  }
+]);
